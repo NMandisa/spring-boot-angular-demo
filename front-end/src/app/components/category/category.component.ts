ef4393a70525d1e7bba0d75ba708ba4335bf994f
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2024-04-07 12:33:14 +0200

    uniformity of design concept

--- front-end/src/app/components/category/category.component.ts
+++ front-end/src/app/components/category/category.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {NavBarComponent} from "../../shared/ui/nav-bar/nav-bar.component";
+import {SignInComponent} from "../../shared/ui/sign-in/sign-in.component";
+import {HeaderComponent} from "../../shared/ui/header/header.component";
+import {FooterComponent} from "../../shared/ui/footer/footer.component";
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
--- front-end/src/app/components/product-list/product-list.component.ts
+++ front-end/src/app/components/product-list/product-list.component.ts
@@ -3,6 +3,10 @@ import {ProductService} from '../../services/product/product.service';
 import {RouterOutlet} from "@angular/router";
 import {LeftSideBarComponent} from "../../shared/ui/left-side-bar/left-side-bar.component";
 import {ProductSideBarComponent} from "../../shared/ui/product-side-bar/product-side-bar.component";
+import {NavBarComponent} from "../../shared/ui/nav-bar/nav-bar.component";
+import {SignInComponent} from "../../shared/ui/sign-in/sign-in.component";
+import {HeaderComponent} from "../../shared/ui/header/header.component";
+import {FooterComponent} from "../../shared/ui/footer/footer.component";
 
 @Component({
   selector: 'app-product-list',
@@ -10,7 +14,11 @@ import {ProductSideBarComponent} from "../../shared/ui/product-side-bar/product-
   imports: [
     RouterOutlet,
     LeftSideBarComponent,
-    ProductSideBarComponent
+    ProductSideBarComponent,
+    NavBarComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
