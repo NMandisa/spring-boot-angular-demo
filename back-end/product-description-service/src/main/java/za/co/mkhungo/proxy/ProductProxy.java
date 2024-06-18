b01f2f7042d282465e9074b1b426d63c3f4d8f85
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2024-06-18 15:55:19 +0200

    drafts | WIP product Module

--- back-end/product-description-service/src/main/java/za/co/mkhungo/proxy/ProductProxy.java
+++ back-end/product-description-service/src/main/java/za/co/mkhungo/proxy/ProductProxy.java
@@ -1,6 +1,9 @@
 package za.co.mkhungo.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import za.co.mkhungo.common.response.ProductDescriptionResponse;
 
 /**
  * @author Noxolo.Mkhungo
@@ -9,5 +12,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 //@FeignClient(name="product-service", url="localhost:8082")
 @FeignClient(name="product-service")
 public interface ProductProxy {
-
+    @PostMapping("/product-service/{id}")
+    ProductDescriptionResponse retrieveProductbyId(@PathVariable("id") Long id);
 }
--- back-end/product-service/src/main/java/za/co/mkhungo/proxy/ProductDescriptionProxy.java
+++ back-end/product-service/src/main/java/za/co/mkhungo/proxy/ProductDescriptionProxy.java
@@ -1,6 +1,10 @@
 package za.co.mkhungo.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import za.co.mkhungo.common.response.ProductDescriptionResponse;
 
 /**
  * @author Noxolo.Mkhungo
@@ -8,4 +12,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 //@FeignClient(name="product-description-service", url="localhost:8094")
 @FeignClient(name="product-description-service")
 public interface ProductDescriptionProxy {
+    @GetMapping("/product-description-service/")
+    ProductDescriptionResponse retrieveProductDesciptions();
+    @PostMapping("/product-description-service/{id}")
+    ProductDescriptionResponse retrieveProductDesciptionById(@PathVariable("id") Long id);
 }
